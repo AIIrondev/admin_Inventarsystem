d3cbb469e0a1ba2218f412e16dc29b3e149c55b6
Maximilian G. <136852512+AIIrondev@users.noreply.github.com>
2025-09-28 19:39:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -546,7 +546,11 @@ sudo ufw default allow outgoing
 # Allow SSH (port 22)
 sudo ufw allow 22
 
-# Allow HTTP (port 8080)
+# Allow HTTPS (port 443)
+sudo ufw allow 443
+
+sudo ufw allow 80
+
 sudo ufw allow 8080
 
 # Enable UFW
